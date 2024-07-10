502515d6b29add74c9347a13a1a70aef3fd0cf5e
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-07-10 17:44:48 -0300

    Update Arrow Functions

--- arrow/ArrowFunctions.ts
+++ arrow/ArrowFunctions.ts
@@ -5,29 +5,38 @@ console.log("Exibir os itens\n");
 /** 
  * Exibir os itens da Lista sem Arrow Functions
  */
-for(let numero of arrayNumeros){
+// for(let numero of arrayNumeros){
 
-    console.log(numero);
+//     console.log(numero);
 
-}
+// }
+
+arrayNumeros.forEach(numero => console.log(numero))
 
 console.log("\nExibir os itens somados com eles mesmos\n");
 
 /** 
  * Exibir os itens da Lista dobrados sem Arrow Functions
  */
-for(let numero of arrayNumeros){
+// for(let numero of arrayNumeros){
 
-    console.log(numero + numero);
+//     console.log(numero + numero);
 
-}
+// }
+
+arrayNumeros.forEach(numero => console.log(numero + numero));
 
 console.log("\nExibir somente os elementos pares da lista\n");
 
 /**
  * Exibir apenas os elementos pares da Lista sem Arrow Functions
  */
-for(let numero of arrayNumeros){
+// for(let numero of arrayNumeros){
+//     if (numero % 2 == 0)
+//         console.log(numero);
+// }
+
+arrayNumeros.forEach(numero => {
     if (numero % 2 == 0)
-        console.log(numero);
-}
+        console.log(numero)
+})
--- arrow/ProcessamentoDados.ts
+++ arrow/ProcessamentoDados.ts
@@ -1,49 +1,79 @@
-﻿let arrayEstados: Array<string> = [
-    "São Paulo",
-    "Rio de Janeiro",
-    "Minas Gerais",
-    "Espirito Santo",
-    "Rio Grande do Sul",
-    "Rio Grande do Norte",
-    "Santa Catarina",
-    "Paraná",
-    "Bahia",
-]
-
-let arrayEstadosRepetidos: Array<string> = [
-    "São Paulo",
-    "Rio de Janeiro",
-    "Minas Gerais",
-    "Espirito Santo",
-    "Rio Grande do Sul",
-    "Rio Grande do Norte",
-    "Santa Catarina",
-    "Paraná",
-    "Rio de Janeiro",
-    "Minas Gerais",
-    "São Paulo",
-    "Santa Catarina",
-    "Paraná",
-    "Bahia",
-]
-
-let numerosOrdenados: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-let numerosAleatorios: Array<number> = [25, 15, 45, 12, 99, 48, 75, 68, 32, 56];
+﻿let numeros: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 let numerosRepetidos: Array<number> = [1, 2, 3, 1, 4, 5, 6, 2, 7, 8, 9, 3, 4, 10, 7];
 
+let numerosDesordenados: Array<number> = [6, 7, 8, 1, 4, 5, 9, 10, 2, 3];
+
+let estados: Array<string> = [
+    "São Paulo",
+    "Rio de Janeiro",
+    "Minas Gerais",
+    "Espirito Santo",
+    "Rio Grande do Sul",
+    "Santa Catarina",
+    "Paraná"
+];
+
+let estadosRepetidos: Array<string> = [
+    'São Paulo',
+    'Rio de Janeiro',
+    'Minas Gerais',
+    'Espirito Santo',
+    'São Paulo',
+    'Rio de Janeiro',
+    'São Paulo',
+    'Rio de Janeiro',
+];
+
+let estadosCentroOeste: Array<string> = [
+    'Goiás',
+    'Mato Grosso',
+    'Mato Grosso do Sul',
+    'Brasília'
+];
+
 console.log("\nmap() - Multiplicar todos os números por 3 e retornar um novo array\n");
+
+let numerosMultiplicadosPor3 = numeros.map( n => n * 3 );
+
+console.dir(numerosMultiplicadosPor3);
 
 console.log("\nfilter() - Listar todos os Estados que contém a palavra Rio\n");
 
+let estadosContemRio = estados.filter( e => e.includes('Rio'));
+
+console.dir(estadosContemRio);
+
 console.log("\nincludes() - Checar se existe na lista o Estado de Minas Gerais\n");
+
+let minasGerais = estados.includes("Minas Gerais");
+
+console.log(minasGerais ? 'Minas Gerais está na lista' : 'Minas Gerais não está na lista');
 
 console.log("\nreduce() - Remover todos os elementos repetidos do array estados\n");
 
+let estadosSemRepeticoes = estadosRepetidos.reduce((acumulador: string[], estado) => {
+    if (acumulador.indexOf(estado) === -1)
+        acumulador.push(estado);
+    return acumulador;
+}, [])
+
+console.dir(estadosSemRepeticoes);
+
 console.log("\nfind() - Encontrar o primeiro número da lista maior do que 9\n");
+
+let numerosMaior9 = numerosRepetidos.find( n => n > 9);
+
+console.log(numerosMaior9);
 
 console.log("\nfindIndex() - Encontrar o Índice do primeiro número da lista maior do que 9\n");
 
-console.log("\nspread - Combinar 2 arrays\n");
+let indiceNumerosMaior9 = numerosRepetidos.findIndex( n => n > 9);
 
+console.log(indiceNumerosMaior9);
+
+console.log("\nSpread Operator - Combinar 2 arrays\n");
+
+const arrayCombinado: number[] = [...numeros, ...numerosDesordenados];
+
+console.dir(arrayCombinado);
